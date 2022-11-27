34a27c6861e351a02a3b87438a224632e980989f
unknown <ojitdebnathfreelancer.com>
2022-11-27 22:43:12 +0600

    admin allseller and all buyers page done

--- src/Layout/DeshboardLayout/AllBuyers/AllBuyers.jsx
+++ src/Layout/DeshboardLayout/AllBuyers/AllBuyers.jsx
@@ -1,9 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllBuyers = () => {
+
+    const { data: buyers = [] } = useQuery({
+        queryKey: ['allbuyers'],
+        queryFn: () => fetch('http://localhost:5000/allbuyers', {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        })
+            .then(res => res.json())
+    });
+
     return (
-        <div>
-            <h1>this is all buyers</h1>
+        <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>buyer Informations</th>
+                        <th>Role</th>
+                        <th className='text-center'>
+                            <button className="btn btn-outline btn-accent btn-sm">Delete All</button>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        buyers.map(buyer => <tr key={buyer._id}>
+                            <td>
+                                {buyer.name}
+                                <br />
+                                <span className="badge badge-ghost badge-sm">{buyer.email}</span>
+                            </td>
+                            <td>{buyer.role}</td>
+                            <th className='text-center'>
+                                <button className="btn btn-outline btn-accent btn-sm">Delete</button>
+                            </th>
+                        </tr>)
+                    }
+                </tbody>
+
+            </table>
         </div>
     );
 };
--- src/Layout/DeshboardLayout/AllSellers/AllSellers.jsx
+++ src/Layout/DeshboardLayout/AllSellers/AllSellers.jsx
@@ -1,9 +1,50 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllSellers = () => {
+    const { data: sellers = [] } = useQuery({
+        queryKey: ['allsellers'],
+        queryFn: () => fetch('http://localhost:5000/allseller', {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        })
+            .then(res => res.json())
+    });
+
     return (
-        <div>
-            <h1>this is all sellers</h1>
+        <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>Seller Informations</th>
+                        <th>Role</th>
+                        <th className='text-center'>Verify</th>
+                        <th className='text-center'>
+                            <button className="btn btn-outline btn-accent btn-sm">Delete All</button>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        sellers.map(seller => <tr key={seller._id}>
+                            <td>
+                                {seller.name}
+                                <br />
+                                <span className="badge badge-ghost badge-sm">{seller.email}</span>
+                            </td>
+                            <td>{seller.role}</td>
+                            <td className='text-center'>
+                                <button className='btn btn-outline btn-primary btn-sm'>Verify</button>
+                            </td>
+                            <th className='text-center'>
+                                <button className="btn btn-outline btn-accent btn-sm">Delete</button>
+                            </th>
+                        </tr>)
+                    }
+                </tbody>
+
+            </table>
         </div>
     );
 };
